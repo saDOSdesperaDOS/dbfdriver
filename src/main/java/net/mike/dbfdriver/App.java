c773ff3f4a5b168ba8a098777830fb86c28339a3
sadosdesperados <bms39rus@gmail.com>
2019-12-12 10:37:11 +0200

    debagg

---
+++ src/main/java/net/mike/dbfdriver/App.java
@@ -0,0 +1,13 @@
+package net.mike.dbfdriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
