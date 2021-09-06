3bb28eaafc9b74f7f096716ba5fd99c36819ce96
sadosdesperados <bms39rus@gmail.com>
2021-09-06 11:51:40 +0200

    refactoring

--- testdrivedrivercsv/src/main/java/net/mike/testdrivedrivercsv/Main.java
+++ testdrivedrivercsv/src/main/java/net/mike/testdrivedrivercsv/Main.java
@@ -9,10 +9,11 @@ import java.text.ParseException;
 
 public class Main {
 	static File dbFile =  new File("d:\\temp\\LM.dbf");
+	static File fdbFile =  new File("d:\\temp\\autoshop\\tw_shop.fdb");
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-		DbfDriver driver = new DbfDriver(dbFile);
+		DbfDriver driver = new DbfDriver(fdbFile);
 		try {
 			driver.readDBF();
 		} catch (IOException e) {
